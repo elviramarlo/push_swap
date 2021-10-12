17c82bdffbc1041a14200c40405ade09a1020784
Elvira Martínez López <elvmarti@MacBook-Pro-de-Carlos.local>
2021-10-12 18:56:22 +0200

    ordena 5 pero es horrible

--- srcs/rule_rrx.c
+++ srcs/rule_rrx.c
@@ -6,7 +6,7 @@
 /*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:19:07 by elvmarti          #+#    #+#             */
-/*   Updated: 2021/10/05 23:00:22 by elvmarti         ###   ########.fr       */
+/*   Updated: 2021/10/11 16:56:46 by elvmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,96 @@
 
 void	rule_rra(t_push_swap **ps)
 {
-	t_listps *lst;
-	int i = 1;
 
-	(*ps)->num_mov++;
-	lst = ft_lstlast_ps((*ps)->stack_a);
-	ft_lstadd_front_ps(&(*ps)->stack_a, ft_lstnew_ps(lst->num));
-	lst = (*ps)->stack_a;
-	while(lst != NULL && i < ft_lstsize_ps(lst) - 1)
+	t_listps	*current;
+	t_listps	*kthNode;
+	int		count;
+	int size = ft_lstsize_ps((*ps)->stack_a) - 1;
+
+	count = 1;
+	if (size != 0)
 	{
-		lst = lst->next;
+		current = (*ps)->stack_a;
+		while (count < size && current != NULL)
+		{
+			current = current->next;
+			count++;
+		}
+		// if (current == NULL)
+		//	return ;
+		kthNode = current;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = (*ps)->stack_a;
+		(*ps)->stack_a = kthNode->next;
+		kthNode->next = NULL;
 	}
-	lst->next = NULL;
-	ft_lstdelone_ps(lst);
+
+/* 	int i = 1;
+	int size;
+	t_listps *lst;
+	
+	(*ps)->num_mov++;
+	if ((*ps)->stack_a)
+	{
+		lst = ft_lstlast_ps((*ps)->stack_a);
+		ft_lstadd_front_ps(&(*ps)->stack_a, ft_lstnew_ps(lst->num));
+		lst = (*ps)->stack_a;
+		size  = ft_lstsize_ps(lst) - 1;
+		while(lst != NULL && i < size)
+		{
+			lst = lst->next;
+			i++;
+		}
+		lst->next = NULL;
+		ft_lstdelone_ps(lst);
+	} */
 }
 
 void	rule_rrb(t_push_swap **ps)
 {
-	t_listps *lst;
+	t_listps	*current;
+	t_listps	*kthNode;
+	int		count;
+	int size = ft_lstsize_ps((*ps)->stack_b) - 1;
+
+	count = 1;
+	if (size != 0)
+	{
+		current = (*ps)->stack_b;
+		while (count < size && current != NULL)
+		{
+			current = current->next;
+			count++;
+		}
+		// if (current == NULL)
+			//return ;
+		kthNode = current;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = (*ps)->stack_b;
+		(*ps)->stack_b = kthNode->next;
+		kthNode->next = NULL;
+	}
+	/* t_listps *lst;
 	int i = 1;
+	int	size;
 
 	(*ps)->num_mov++;
-	lst = ft_lstlast_ps((*ps)->stack_b);
-	ft_lstadd_front_ps(&(*ps)->stack_b, ft_lstnew_ps(lst->num));
-	lst = (*ps)->stack_b;
-	while(lst != NULL && i < ft_lstsize_ps(lst) - 1)
+	if ((*ps)->stack_b)
 	{
-		lst = lst->next;
-	}
-	lst->next = NULL;
-	ft_lstdelone_ps(lst);
+		lst = ft_lstlast_ps((*ps)->stack_b);
+		ft_lstadd_front_ps(&(*ps)->stack_b, ft_lstnew_ps(lst->num));
+		size = ft_lstsize_ps((*ps)->stack_b) - 1;
+		lst = (*ps)->stack_b;
+		while(lst != NULL && i < size)
+		{
+			lst = lst->next;
+			i++;
+		}
+		lst->next = NULL;
+		ft_lstdelone_ps(lst);
+	} */
 }
 
 void	rule_rrr(t_push_swap **ps)
--- srcs/sort_small.c
+++ srcs/sort_small.c
@@ -6,17 +6,116 @@
 /*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 17:12:23 by elvmarti          #+#    #+#             */
-/*   Updated: 2021/10/08 17:20:07 by elvmarti         ###   ########.fr       */
+/*   Updated: 2021/10/12 17:37:53 by elvmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+static void	sort_three(t_push_swap *ps)
+{
+	int	one;
+	int	two;
+	int	three;
+
+	one = ps->stack_a->num;
+	two = ps->stack_a->next->num;
+	three = ps->stack_a->next->next->num;
+	if (one > two && two < three && one < three) //2 1 3
+		rule_sa(ps);
+	else if (one > two && two < three && one > three) //3 1 2
+		rule_ra(&ps);
+	else if (one < two && two > three && one < three) //1 3 2
+	{
+		rule_sa(ps);
+		rule_ra(&ps);
+	}
+	else if (one > two && two > three && one > three) //3 2 1
+	{
+		rule_ra(&ps);
+		rule_sa(ps);
+	}
+	else if (one < two && two > three && one > three) //2 3 1
+		rule_rra(&ps);
+}
+
+static void	sort_four(t_push_swap *ps, int size)
+{
+	while(size > 3)
+	{
+		int pos1 = ps->stack_a->num;
+		int pos2 = ps->stack_a->next->num;
+		int pos3 = ps->stack_a->next->next->num;
+		int pos4 = ps->stack_a->next->next->next->num;
+
+		if (pos1 < pos2 && pos1 < pos3 && pos1 < pos4)
+		{
+			rule_pb(&ps);
+			size--;
+		}
+		else if (pos1 > pos2)
+			rule_sa(ps);
+		else if (pos1 > pos3)
+		{
+			rule_ra(&ps);
+			rule_ra(&ps);
+		}
+		else if (pos1 > pos4)
+		{
+			rule_rra(&ps);
+			rule_rra(&ps);
+		}
+	}
+	sort_three(ps);
+	rule_pa(&ps);
+	rule_pa(&ps);
+}
+
+static void	sort_five(t_push_swap *ps, int size)
+{
+	while (size > 4)  
+	{
+		int pos1 = ps->stack_a->num;
+		int pos2 = ps->stack_a->next->num;
+		int pos3 = ps->stack_a->next->next->num;
+		int pos4 = ps->stack_a->next->next->next->num;
+		int pos5 = ps->stack_a->next->next->next->next->num;
+
+		if (pos1 < pos2 && pos1 < pos3 && pos1 < pos4 && pos1 < pos5)
+		{
+			rule_pb(&ps);
+			size--;
+		}
+		else if (pos1 > pos2)
+			rule_sa(ps);
+		else if (pos1 > pos3)
+		{
+			rule_ra(&ps);
+			rule_ra(&ps);
+		}
+		else if (pos1 > pos4)
+		{
+			rule_rra(&ps);
+			rule_rra(&ps);
+		}
+		else if (pos1 > pos5)
+			rule_rra(&ps);
+	}
+	sort_four(ps, size);
+	
+}
+
 void	sort_small_stack(t_push_swap *ps)
 {
-	int i;
+	int			size;
 
-	i =  ft_lstsize_ps(ps->stack_a);
-
-
+	size =  ft_lstsize_ps(ps->stack_a);
+	if (size == 2)
+		rule_sa(ps);
+	if (size == 3)
+		sort_three(ps);
+	if (size == 4)
+		sort_four(ps, size);
+	if (size == 5)
+		sort_five(ps, size);
 }
